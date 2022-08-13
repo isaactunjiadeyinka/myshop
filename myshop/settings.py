e7114861520bc64a242799b4531e6a74b4a142a7
Isaac Adeyinka <isaac@debian11.Adeyinkasolutions.org>
2022-08-14 03:29:35 +1200

    New updates for Heroku deployment

--- myshop/settings.py
+++ myshop/settings.py
@@ -130,3 +130,5 @@ CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://chicandclassy.herokuapp.com/']
