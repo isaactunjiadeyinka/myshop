219e65d870e00305632fa6cf5d610a37d7587378
Isaac Adeyinka <isaac@debian11.Adeyinkasolutions.org>
2022-08-14 02:59:01 +1200

    New updates for Heroku deployment

--- myshop/settings.py
+++ myshop/settings.py
@@ -129,3 +129,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
